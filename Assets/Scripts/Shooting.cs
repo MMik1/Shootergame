c6882d35526167e3c0ccc5647c7f507dd0f66744
MMik1 <m.mik@sintlucasedu.nl>
2023-12-04 12:53:49 +0100

    64

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,33 +1,88 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
     public Camera cam;
+    public GameObject muzzleFlashPrefab;
+    public int maxAmmo = 10;
+    public int reserveAmmo = 50;
+    public AudioSource reloadSound;
+
+    private int currentAmmo;
+    private bool isReloading = false;
 
     private RaycastHit hit;
     private Ray ray;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        currentAmmo = maxAmmo;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) //lmb
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && currentAmmo > 0)
+        {
+            Shoot();
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo && reserveAmmo > 0)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    void Shoot()
+    {
+        if (currentAmmo > 0)
         {
             ray = cam.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit))
+
+            if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.tag.Equals("Enemy"))
+                if (hit.collider.CompareTag("Enemy"))
                 {
                     Destroy(hit.collider.gameObject);
                     Debug.Log("Tymon = mijn poekiebeeertje");
-                }                
+
+                    Instantiate(muzzleFlashPrefab, hit.point, Quaternion.identity);
+
+                    currentAmmo--;
+                }
             }
         }
+        else
+        {
+            Debug.Log("Out of ammo!");
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        if (reloadSound != null)
+        {
+            reloadSound.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Reload sound not assigned!");
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        
+        int ammoToFill = Mathf.Min(maxAmmo - currentAmmo, reserveAmmo);
+        currentAmmo += ammoToFill;
+        reserveAmmo -= ammoToFill;
+
+        isReloading = false;
     }
 }
